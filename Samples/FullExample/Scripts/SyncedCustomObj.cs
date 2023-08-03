2f20f79e896da41725996f96edfe6fb59caddcb9
Anton Franzluebbers <anton@uga.edu>
2023-08-03 13:07:40 -0400

    added takeownership editor button, improved samples, add IPackState interface to serialization scripts

---
+++ Samples/FullExample/Scripts/SyncedCustomObj.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using VelNet;
+
+public class SyncedCustomObj : NetworkSerializedObjectStream
+{
+	private Renderer rend;
+	private Rigidbody rb;
+	public float impulseForce = 1;
+	public float returningForce = 1;
+
+	private IEnumerator Start()
+	{
+		rend = GetComponent<Renderer>();
+		rb = GetComponent<Rigidbody>();
+
+		while (true)
+		{
+			if (IsMine)
+			{
+				rb.AddForce(new Vector3(
+					Random.Range(-1f, 1f) * impulseForce,
+					Random.Range(-1f, 1f) * impulseForce,
+					Random.Range(-1f, 1f) * impulseForce
+				));
+			}
+
+			yield return new WaitForSeconds(Random.Range(1f, 3f));
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		if (IsMine)
+		{
+			// return to center
+			rb.velocity -= (transform.position).normalized * Time.fixedDeltaTime * returningForce;
+		}
+	}
+
+	protected override void SendState(BinaryWriter binaryWriter)
+	{
+	}
+
+	protected override void ReceiveState(BinaryReader binaryReader)
+	{
+	}
+}
