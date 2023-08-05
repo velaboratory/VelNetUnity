bd7be1e191bdcae9191b089677e674ecac09b61c
Anton Franzluebbers <anton@uga.edu>
2023-08-05 15:19:45 -0400

    clean up ci

--- Documentation/astro.config.mjs
+++ Documentation/astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()]
+  integrations: [mdx(), tailwind()],
+  output: "server",
+  adapter: cloudflare()
 });
